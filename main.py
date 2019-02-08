7ad477dcf20f9e7cf9778de5e32accc52ad3c4db
eqblog <dgood@email.axc.edu.gr>
2019-02-08 18:56:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re,requests,js2py
 def bypass_cloudflare(url_w):
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
     req=requests.Session()
